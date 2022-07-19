4abf0052bdfac18785acb9a570284f0719951c09
lvsouza <ls18017@gmail.com>
2022-07-19 08:13:12 -0300

    feat: added yup validation to cidades create controller

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,35 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  console.log(req.body);
+const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+
+  console.log(validatedData);
 
 
 
