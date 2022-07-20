2117d867f0eacc979eb14927f525eade7fe8cb09
lvsouza <ls18017@gmail.com>
2022-07-20 12:57:38 -0300

    feat: added body validation middleware

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,24 +1,20 @@
-import { Request, Response } from 'express';
+import { Request, RequestHandler, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
-
 
 
 interface ICidade {
   nome: string;
   estado: string;
 }
-
 const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
   estado: yup.string().required().min(3),
 });
-
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-  let validatedData: ICidade | undefined = undefined;
-
+export const createBodyValidator: RequestHandler = async (req, res, next) => {
   try {
-    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+    await bodyValidation.validate(req.body, { abortEarly: false });
+    return next();
   } catch (err) {
     const yupError = err as yup.ValidationError;
     const errors: Record<string, string> = {};
@@ -30,14 +26,35 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
 
     return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
-
-  console.log(validatedData);
-
+};
 
 
+interface IFilter {
+  filter?: string;
+}
+const queryValidation: yup.SchemaOf<IFilter> = yup.object().shape({
+  filter: yup.string().required().min(3),
+});
+export const createQueryValidator: RequestHandler = async (req, res, next) => {
+  try {
+    await queryValidation.validate(req.query, { abortEarly: false });
+    return next();
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+  }
+};
 
 
-
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  console.log(req.body);
 
 
   return res.send('Create!');
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -13,7 +13,12 @@ router.get('/', (_, res) => {
   return res.send('Olá, DEV!');
 });
 
-router.post('/cidades', CidadesController.create);
+router.post(
+  '/cidades',
+  CidadesController.createBodyValidator,
+  CidadesController.createQueryValidator,
+  CidadesController.create
+);
 
 
 
