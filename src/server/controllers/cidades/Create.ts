7ffd2b09891c088cae63ba981c9bf67bdbcb0082
lvsouza <ls18017@gmail.com>
2022-07-25 13:35:09 -0300

    feat: added tests to create method in cidades controller

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -17,5 +17,5 @@ export const createValidation = validation((getSchema) => ({
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   console.log(req.body);
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!');
+  return res.status(StatusCodes.CREATED).json(1);
 };
---
+++ tests/cidades/Create.test.ts
@@ -0,0 +1,29 @@
+import { StatusCodes } from 'http-status-codes';
+import { testServer } from '../jest.setup';
+
+
+describe('Cidades - Create', () => {
+
+
+  it('Cria registro', async () => {
+
+    const res1 = await testServer
+      .post('/cidades')
+      .send({ nome: 'Caxias do Sul' });
+
+
+    expect(res1.statusCode).toEqual(StatusCodes.CREATED);
+    expect(typeof res1.body).toEqual('number');
+  });
+  it('Tenta criar um registro com nome muito curto', async () => {
+
+    const res1 = await testServer
+      .post('/cidades')
+      .send({ nome: 'Ca' });
+
+
+    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(res1.body).toHaveProperty('errors.body.nome');
+  });
+});
+
