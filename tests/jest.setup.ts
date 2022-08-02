d285634a5343da561e126b079fb2d402d5504c4e
lvsouza <ls18017@gmail.com>
2022-08-02 07:22:44 -0300

    fix: improved jest setup to auto migrate knex database

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,6 +1,16 @@
 import supertest from 'supertest';
+import { Knex } from '../src/server/database/knex';
 
 import { server } from '../src/server/Server';
+
+
+beforeAll(async () => {
+  await Knex.migrate.latest();
+});
+
+afterAll(async () => {
+  await Knex.destroy();
+});
 
 
 export const testServer = supertest(server);
