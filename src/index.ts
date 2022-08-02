c955ca2184e179bdecb4529da2b6b2c09ee24ba8
lvsouza <ls18017@gmail.com>
2022-08-02 19:27:19 -0300

    refactor: added auto run for migrations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/Server';
 
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
+};
+
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  console.log('Rodando migrations');
+
+  Knex.migrate
+    .latest()
+    .then(() => startServer())
+    .catch(console.log);
+} else {
+  startServer();
+}
