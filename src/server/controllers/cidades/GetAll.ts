e1cc84fc560346e2490eb898ddbeac1bcc3bbd01
lvsouza <ls18017@gmail.com>
2022-08-13 14:54:48 -0300

    fix: improved query.id

--- src/server/controllers/cidades/GetAll.ts
+++ src/server/controllers/cidades/GetAll.ts
@@ -22,7 +22,7 @@ export const getAllValidation = validation((getSchema) => ({
 }));
 
 export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
-  const result = await CidadesProvider.getAll(req.query.page || 1, req.query.limit || 7, req.query.filter || '', Number(req.query.id));
+  const result = await CidadesProvider.getAll(req.query.page || 1, req.query.limit || 7, req.query.filter || '', Number(req.query.id || 0));
   const count = await CidadesProvider.count(req.query.filter);
 
   if (result instanceof Error) {
