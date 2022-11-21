a0c08df82a1c80e8c6dfc8b3ada37dda3ddfdf2b
lvsouza <ls18017@gmail.com>
2022-11-21 06:54:01 -0300

    fix: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/Server';
 
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
+};
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate.latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
